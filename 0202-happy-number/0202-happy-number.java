d061afeeddea326b0b5666a04d6a79a1e7d1776e
Niraj Kumar Modi <niraj.kumar.modi.74@gmail.com>
2026-07-01 11:20:38 +0530

    Time: 0 ms (100%), Space: 42.2 MB (88.92%) - LeetHub

--- 0202-happy-number/0202-happy-number.java
+++ 0202-happy-number/0202-happy-number.java
@@ -1,23 +1,20 @@
 class Solution {
-    private int getSum(int n) {
-        int sum = 0;
 
-        while (n > 0) {
-            int d = n % 10;
-            sum += d * d;
-            n /= 10;
-        }
-
-        return sum;
-    }
     public boolean isHappy(int n) {
-        int slow = n;
-        int fast = n;
-        do {
-            slow = getSum(slow);          // 1 step
-            fast = getSum(getSum(fast));  // 2 steps 
-        } while (slow != fast);
-
-        return slow == 1;
+        while (n != 4) {
+            int num = n;
+            int sum = 0;
+            while (num > 0) {
+                int d = num % 10;
+                sum += d * d;
+                num /= 10;
+            }
+            if (sum == 1) {
+                return true;
+            } else {
+                n = sum;
+            }
+        }
+        return false;
     }
 }
