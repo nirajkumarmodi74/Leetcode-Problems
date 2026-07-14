3a57429f1fcf70f15791a97b543ce82e513942d8
Niraj Kumar Modi <niraj.kumar.modi.74@gmail.com>
2026-07-14 20:29:59 +0530

    Time: 1 ms (99%), Space: 42.7 MB (33%) - LeetHub

--- 0263-ugly-number/0263-ugly-number.java
+++ 0263-ugly-number/0263-ugly-number.java
@@ -2,16 +2,12 @@ class Solution {
     public boolean isUgly(int n) {
         if(n<=0)
             return false;
-        
         while(n%2==0)
-            n/=2;
-        
+            n=n/2;
         while(n%3==0)
-            n/=3;
-        
+            n=n/3;
         while(n%5==0)
             n/=5;
-        
-        return n==1;
+    return n==1;
     }
 }
