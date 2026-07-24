4083293f34ac495be76b9e6c9b8a5d6dc3b90357
Niraj Kumar Modi <niraj.kumar.modi.74@gmail.com>
2026-07-24 22:25:07 +0530

    Time: 2 ms (74.78%), Space: 43.9 MB (50.92%) - LeetHub

--- 3174-clear-digits/3174-clear-digits.java
+++ 3174-clear-digits/3174-clear-digits.java
@@ -1,17 +1,12 @@
 class Solution {
     public String clearDigits(String s) {
-        Stack<Character> w = new Stack<>();
-        for(char ch : s.toCharArray()){
-            int t = ch - '0';
-            if(t>=0 && t<=9){
-                w.pop();
-            }else{
-                w.push(ch);
-            }
-        }
         StringBuilder word = new StringBuilder();
-        for(char ch : w){
-            word.append(ch);
+        for(char ch : s.toCharArray()){
+            if(Character.isDigit(ch)){
+                word.deleteCharAt(word.length()-1);
+            }else{
+                word.append(ch);
+            }
         }
         return word.toString();
     }
