4b67bd27c5686e6fd648029bef2bc150932e3df6
Niraj Kumar Modi <niraj.kumar.modi.74@gmail.com>
2026-08-28 03:51:49 +0530

    Time: 0 ms (100%), Space: 42.3 MB (15.98%) - LeetHub

--- 0509-fibonacci-number/0509-fibonacci-number.java
+++ 0509-fibonacci-number/0509-fibonacci-number.java
@@ -6,7 +6,12 @@ class Solution {
         if(n==1){
             return 1;
         }
-        int ans  = fib(n-2)+fib(n-1);
-        return ans;
+        int[] dp = new int[n+1];
+        dp[0] = 0;
+        dp[1] = 1;
+        for(int i =2;i<=n;i++){
+            dp[i] = dp[i-2]+dp[i-1];
+        }
+        return dp[n];
     }
 }
